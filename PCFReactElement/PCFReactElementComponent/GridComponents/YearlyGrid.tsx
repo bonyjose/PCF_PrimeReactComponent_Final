ee907de36741f3f9fe2cdd7cce3f1c07c881621b
Nijomon <nijomon.jose@teranet.ca>
2020-05-05 14:11:33 +0530

    made cells editable

--- PCFReactElement/PCFReactElementComponent/GridComponents/YearlyGrid.tsx
+++ PCFReactElement/PCFReactElementComponent/GridComponents/YearlyGrid.tsx
@@ -8,6 +8,8 @@ import { Dropdown } from "primereact/dropdown";
 // import "./CSS/primeicons.css";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+
+import {InputText} from 'primereact/inputtext';
  
 interface Props {}
 interface State {
@@ -15,53 +17,67 @@ interface State {
   SelectedLayout: string;
 }
  export class GridYearlyComponent extends React.Component {
-    products: { id: string; name: string; place: string; price: string }[];
+   clonedCars: {};
+   products: { id: string; name: string; place: string; price: string; }[];
     constructor(props: Props) {
       super(props);
-      this.products = [
-        {
-          id: "1",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-        {
-          id: "2",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-        {
-          id: "3",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-        {
-          id: "4",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-        {
-          id: "5",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-        {
-          id: "6",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-        {
-          id: "7",
-          name: "nijomon",
-          place: "Kply",
-          price: "30",
-        },
-      ];
+      this.state = {
+       
+    };
+    this.products = [
+      {
+        id: "1",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+      {
+        id: "2",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+      {
+        id: "3",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+      {
+        id: "4",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+      {
+        id: "5",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+      {
+        id: "6",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+      {
+        id: "7",
+        name: "nijomon",
+        place: "Kply",
+        price: "30",
+      },
+    ]
+     
+
+
+      this.clonedCars = {};
+
+      this.vinEditor = this.vinEditor.bind(this);
+      this.yearEditor = this.yearEditor.bind(this);
+      this.brandEditor = this.brandEditor.bind(this);
+      this.colorEditor = this.colorEditor.bind(this);
+      this.requiredValidator = this.requiredValidator.bind(this);
     }
   
     render() {
@@ -70,13 +86,48 @@ interface State {
           value={this.products}
           paginator={true}
           rows={5}
+          editMode ="Cell"
           rowsPerPageOptions={[5, 10, 30]}
         >
-          <Column field="id" header="ID" />
-          <Column field="name" header="Name" />
-          <Column field="PRice" header="Price" />
-          <Column field="place" header="place" />
+          <Column field="id" header="ID"  editor={this.vinEditor} editorValidator={this.requiredValidator} style={{height: '3.5em'}}/>
+          <Column field="name" header="Name" editor={this.yearEditor} style={{height: '3.5em'}}/>
+          <Column field="Price" header="Price"   editor={this.brandEditor} style={{height: '3.5em'}} />
+          <Column field="place" header="place"  editor={this.colorEditor} style={{height: '3.5em'}} />
         </DataTable>
       );
     }
+
+
+
+     /* Cell Editing */
+     onEditorValueChange(props, value) {
+      let updatedCars = [...props.value];
+      updatedCars[props.rowIndex][props.field] = value;
+      this.setState({cars1: updatedCars});
+  }
+
+  inputTextEditor(props, field) {
+      return <InputText type="text" value={props.rowData[field]} onChange={(e) => this.onEditorValueChange(props, e.currentTarget.value)} />;
+  }
+
+  vinEditor(props) {
+      return this.inputTextEditor(props, 'id');
+  }
+
+  yearEditor(props) {
+      return this.inputTextEditor(props, 'name');
+  }
+
+  brandEditor(props) {
+    return this.inputTextEditor(props, 'price');
+  }
+
+  colorEditor(props) {
+      return this.inputTextEditor(props, 'place');
+  }
+
+  requiredValidator(props) {
+      let value = props.rowData[props.field];
+      return value && value.length > 0;
+  }
   }
