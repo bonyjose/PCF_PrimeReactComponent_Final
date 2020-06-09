63e8d9e422abcca949fe656d6c4ef12623125e85
Nijomon <nijomon.jose@teranet.ca>
2020-06-09 20:27:05 +0530

    Quarter component updated

--- PCFReactElement/PCFReactElementComponent/GridComponents/QuarterlyGrid.tsx
+++ PCFReactElement/PCFReactElementComponent/GridComponents/QuarterlyGrid.tsx
@@ -82,7 +82,15 @@ interface State {
           data[i].Q4 = q4 == 0 ? '': "$" + q4.toFixed(2);
           if(data[i][lineTot] !== null && typeof (data[i][lineTot]) !== 'undefined' && data[i][lineTot] !=="")
           {
-            data[i][lineTot] = data[i][lineTot] == 0 ? '': "$" + data[i][lineTot].toFixed(2);
+            if(data[i][lineTot] !==0)
+            {
+              data[i][lineTot] = "$" + this.numberTryParse(data[i][lineTot]).toFixed(2);
+            }
+            else
+            {
+              data[i][lineTot] =  "$" + 0.00;
+            }
+            
           }
           console.log(data[i]);
           i++;
